1fb82ad777232feb99233fadaef18d5161b9b5bc
pedablio <paulohrl96@gmail.com>
2022-04-12 09:14:30 -0300

    feat: Update with webview navigation

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
+import {BackHandler} from 'react-native';
 import {WebView} from 'react-native-webview';
 
 function App() {
+  const [canGoBack, setCanGoBack] = useState(false);
+  const webViewRef = useRef(null);
+
+  function handleNavigation({nativeEvent}) {
+    setCanGoBack(nativeEvent.canGoBack);
+  }
+
+  useEffect(() => {
+    function handleBack() {
+      if (canGoBack && webViewRef.current) {
+        webViewRef.current.goBack();
+      } else {
+        BackHandler.exitApp();
+      }
+
+      return true;
+    }
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      handleBack,
+    );
+
+    return () => backHandler.remove();
+  }, [canGoBack]);
+
   return (
     <WebView
-      geolocationEnabled={true}
-      source={{
-        uri: 'https://campinas.sistemaexpert.com.br/usuario/login/',
-      }}
+      ref={webViewRef}
+      source={{uri: 'https://campinas-teste.sistemaexpert.com.br/usuario/campinas/'}}
+      onLoadProgress={handleNavigation}
     />
   );
 }
